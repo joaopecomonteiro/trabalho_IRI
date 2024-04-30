5bee1402ec06bb06416c2440b356b59d8eadb137
joaomonteiro <joaopecomonteiro@gmail.com>
2024-04-30 15:27:42 +0100

    changes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if platform.system() == "Linux":
 
 
 from controller import Robot, Supervisor, GPS, Compass
-from utils import warp_robot
+from utils import warp_robot, move_robot_to
 
 import numpy as np
 import math
@@ -35,11 +35,23 @@ def main() -> None:
     warp_robot(supervisor, "EPUCK", robot_position)
 
 
-
+    moves = [(0, 0), (0, 1), (0, 2),
+             (1, 2), (1, 1), (1, 0),
+             (2, 0), (2, 1), (2, 2)]
 
 
     while supervisor.step() != -1:
-        pass
+        step_distance: float = 1
+        #new_position: (float, float) = (robot_position[0] + step_distance,
+        #                                robot_position[1] + step_distance)
+        new_position = moves.pop(0)
+        print(new_position)
+        move_robot_to(supervisor, robot_position, robot_orientation, new_position, 0.1, math.pi)
+
+        gps_readings = gps.getValues()
+        robot_position = (gps_readings[0], gps_readings[1])
+        compass_readings = compass.getValues()
+        robot_orientation = math.atan2(compass_readings[0], compass_readings[1])
 
 
 if __name__ == '__main__':
