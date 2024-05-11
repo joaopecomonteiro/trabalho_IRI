796358f89c6492e4335e5ed852eb4af4e51eeff8
Ana Pinto <anapinto22012003@gmail.com>
2024-05-11 23:01:45 +0100

    update w new map

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from utils import warp_robot, move_robot_to, bresenham
 from transformations import get_translation, create_tf_matrix
 
 
-
+from tqdm import tqdm
 import numpy as np
 import math
 import pdb
@@ -20,7 +20,6 @@ import open3d as o3d
 import pyransac3d as pyrsc
 
 def main() -> None:
-
     supervisor: Supervisor = Supervisor()
 
     timestep: int = int(supervisor.getBasicTimeStep())  # in ms
@@ -45,12 +44,10 @@ def main() -> None:
 
     data = []
 
-    moves = [(0.10, 0.10), (0.10, 0.40), (0.10, 0.70),
-             (0.40, 0.70), (0.70, 0.70),
-             (0.70, 0.40), (0.70, 0.10)]
 
-    x = np.arange(0, 0.7, 0.2)
-    y = np.arange(0, 0.7, 0.2)
+
+    x = np.arange(0, 4, 0.3)
+    y = np.arange(0, 4, 0.2)
     moves = [(tx, ty) for tx in x for ty in y]
     print(f"number of moves: {len(moves)}")
     #moves = [(0.10, 0.10)]
@@ -62,7 +59,7 @@ def main() -> None:
         #                                robot_position[1] + step_distance)
         new_position = moves.pop(0)
         print(new_position)
-        move_robot_to(supervisor, robot_position, robot_orientation, new_position, 0.1, math.pi)
+        move_robot_to(supervisor, robot_position, robot_orientation, new_position, 1, math.pi)
 
         gps_readings = gps.getValues()
         robot_position = (gps_readings[0], gps_readings[1])
@@ -111,32 +108,51 @@ def main() -> None:
     #cylinder = pyrsc.Cylinder()
     #center, axis, radius, inliers = cylinder.fit(data_arr, 0.5)
 
-    cuboid = pyrsc.Cuboid()
+    # cuboid1 = pyrsc.Cuboid()
+    # cuboid2 = pyrsc.Cuboid()
+    #
+    # best_eq1, inliers_cuboid1 = cuboid1.fit(data_arr, 0.005)
+    #
+    # print(f"number of inliers cuboid: {len(inliers_cuboid1)}")
+    #
+    #
+    # inliers_cuboid1 = data_arr[inliers_cuboid1, :]
+    # outliers_cuboid1 = np.array([point for point in tqdm(data_arr) if point not in inliers_cuboid1])
+    # print(outliers_cuboid1.shape)
+    #
+    # point_cloud = o3d.geometry.PointCloud()
+    # point_cloud.points = o3d.utility.Vector3dVector(inliers_cuboid1)
+    # o3d.visualization.draw_plotly([point_cloud])
+    #
+    # #cylinder = pyrsc.Cylinder()
+    # #center, axis, radius, inliers_cylinder = cylinder.fit(inliers_cuboid, 0.001)
+    # #inliers_cylinder = inliers_cuboid[inliers_cylinder, :]
+    #
+    # best_eq2, inliers_cuboid2 = cuboid2.fit(outliers_cuboid1, 0.001)
+    # #pdb.set_trace()
+    # inliers_cuboid2 = outliers_cuboid1[inliers_cuboid2, :]
+    # outliers_cuboid2 = np.array([point for point in tqdm(outliers_cuboid1) if point not in inliers_cuboid2])
+    #
+    # print(f"number of inliers cylinder : {len(inliers_cuboid2)}")
+    #
+    #
+    # point_cloud = o3d.geometry.PointCloud()
+    # point_cloud.points = o3d.utility.Vector3dVector(inliers_cuboid2)
+    # o3d.visualization.draw_plotly([point_cloud])
 
-    best_eq, inliers_cuboid = cuboid.fit(data_arr, 0.0005)
+    outliers_before = data_arr
+    for i in range(9):
+        plane = pyrsc.Plane()
 
-    print(f"number of inliers cuboid: {len(inliers_cuboid)}")
+        equation, inliers_plane = plane.fit(outliers_before, 0.00001, minPoints=2000, maxIteration=100000)
+        inliers_plane = outliers_before[inliers_plane, :]
+        outliers_plane = np.array([point for point in tqdm(outliers_before) if point not in inliers_plane])
+        print(inliers_plane.shape)
+        point_cloud = o3d.geometry.PointCloud()
+        point_cloud.points = o3d.utility.Vector3dVector(inliers_plane)
+        o3d.visualization.draw_plotly([point_cloud])
 
-
-    inliers_cuboid = data_arr[inliers_cuboid, :]
-
-
-    point_cloud = o3d.geometry.PointCloud()
-    point_cloud.points = o3d.utility.Vector3dVector(inliers_cuboid)
-    o3d.visualization.draw_plotly([point_cloud])
-
-    cylinder = pyrsc.Cylinder()
-    center, axis, radius, inliers_cylinder = cylinder.fit(inliers_cuboid, 0.0005)
-
-    print(f"number of inliers cylinder: {len(inliers_cylinder)}")
-
-    inliers_cylinder = inliers_cuboid[inliers_cylinder, :]
-
-    point_cloud = o3d.geometry.PointCloud()
-    point_cloud.points = o3d.utility.Vector3dVector(inliers_cylinder)
-    o3d.visualization.draw_plotly([point_cloud])
-
-
+        outliers_before = outliers_plane
     pdb.set_trace()
 
 if __name__ == '__main__':
