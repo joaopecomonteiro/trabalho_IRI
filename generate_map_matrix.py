83375140472fa9589ec80e753368c87965bf4891
Tomás R <tomas.o.rodrigues@gmail.com>
2024-06-12 14:40:28 +0100

    Gerador de mapa atualizado

--- generate_map_matrix.py
+++ generate_map_matrix.py
@@ -17,6 +17,7 @@ wall_counter = 0
 
 angles_dict = {
     'draw_triangle': [-60, 60],
+    'draw_rectangle': [-45, 45],
     'draw_square': [-45, 45],
     'draw_pentagon': [-36, 36]
 }
@@ -115,19 +116,81 @@ def write_webots_world(point1, point2):
 
 #==================================================================================================
 
-def draw_square(matrix, mask, shapes, top_left, size, angle=0, tresh_matrix=None, threshold=300, placed=None):
+# function to draw a square
+def draw_square(matrix, mask, shapes, top_left, size, angle=0, thresh_mask=None, threshold=300, placed=None):
     x, y = top_left
-
-    # Define the vertices of the square
-    #if x + size > matrix.shape[0] - threshold or x < threshold or y < threshold or y + size > matrix.shape[
-    #    0] - threshold:
-    #    matrix += 1000
-
 
     A = (x, y)
     B = (x + size, y)
     C = (x + size, y + size)
     D = (x, y + size)
+    #print(A, B, C, D)
+    # Calculate the centroid of the square
+    vertices = np.array([A, B, C, D])
+    center = tuple(np.mean(vertices, axis=0).astype(int))
+
+    # Rotate the vertices around the centroid
+    A_rot = rotate_point(center, A, angle)
+    B_rot = rotate_point(center, B, angle)
+    C_rot = rotate_point(center, C, angle)
+    D_rot = rotate_point(center, D, angle)
+
+    if placed:
+        write_webots_world(A_rot, B_rot)
+        write_webots_world(B_rot, C_rot)
+        write_webots_world(C_rot, D_rot)
+        write_webots_world(D_rot, A_rot)
+
+        f_csv = open(custom_maps_filepath + map_name + '_points.csv', 'w', newline='')
+        writer = csv.writer(f_csv)
+
+    #print(A_rot, B_rot, C_rot, D_rot)
+    # Draw the rotated square
+    cv2.line(matrix, A_rot, B_rot, color=(255, 255, 255), thickness=10)
+    cv2.line(matrix, B_rot, C_rot, color=(255, 255, 255), thickness=10)
+    cv2.line(matrix, C_rot, D_rot, color=(255, 255, 255), thickness=10)
+    cv2.line(matrix, D_rot, A_rot, color=(255, 255, 255), thickness=10)
+
+    # Create and fill the polygon
+    polygon = np.array([A_rot, B_rot, C_rot, D_rot], np.int32)
+    #breakpoint()
+    #rr, cc = draw.polygon(polygon[:, 1], polygon[:, 0], mask.shape)
+    #mask[rr, cc] = 1
+
+    cv2.fillConvexPoly(mask, polygon, 1)
+    shapes.append(["square", polygon, angle, center])
+    #if placed:
+    #    breakpoint()
+
+    if thresh_mask is not None:
+
+        A_exp, B_exp, C_exp, D_exp = expand_geometric_figure([A_rot, B_rot, C_rot, D_rot], 1000)
+
+        if any(x < threshold or x > thresh_mask.shape[0] - threshold or y < threshold or y > thresh_mask.shape[1] - threshold for x, y in [A_exp, B_exp, C_exp, D_exp]):
+            thresh_mask += 1000
+
+        #print(A_exp, B_exp, C_exp, D_exp)
+        polygon_exp = np.array([A_exp, B_exp, C_exp, D_exp], np.int32)
+        #print("im here")
+        #breakpoint()
+        #rr, cc = draw.polygon(polygon_exp[:, 1], polygon_exp[:, 0], thresh_mask.shape)
+        #thresh_mask[rr, cc] = 1
+
+
+        cv2.fillConvexPoly(thresh_mask, polygon_exp, 1)
+    #print("okok")
+    return shapes
+
+# function to draw a rectangle
+def draw_rectangle(matrix, mask, shapes, top_left, size, angle=0, thresh_mask=None, threshold=300, placed=None):
+    x, y = top_left
+
+    shorter_side = random.randint(int(size/4), int(size*(3/4)))
+
+    A = (x, y)
+    B = (x + size, y)
+    C = (x + size, y + shorter_side)
+    D = (x, y + shorter_side)
     #print(A, B, C, D)
     # Calculate the centroid of the square
     vertices = np.array([A, B, C, D])
@@ -165,31 +228,30 @@ def draw_square(matrix, mask, shapes, top_left, size, angle=0, tresh_matrix=None
     #mask[rr, cc] = 1
 
     cv2.fillConvexPoly(mask, polygon, 1)
-    shapes.append(["square", polygon, angle, center])
+    shapes.append(["rectangle", polygon, angle, center])
     #if placed:
     #    breakpoint()
 
-    if tresh_matrix is not None:
+    if thresh_mask is not None:
 
         A_exp, B_exp, C_exp, D_exp = expand_geometric_figure([A_rot, B_rot, C_rot, D_rot], 1000)
 
-        if any(x < threshold or x > tresh_matrix.shape[0] - threshold or y < threshold or y > tresh_matrix.shape[1] - threshold for x, y in [A_exp, B_exp, C_exp, D_exp]):
-            tresh_matrix += 1000
+        if any(x < threshold or x > thresh_mask.shape[0] - threshold or y < threshold or y > thresh_mask.shape[1] - threshold for x, y in [A_exp, B_exp, C_exp, D_exp]):
+            thresh_mask += 1000
 
         #print(A_exp, B_exp, C_exp, D_exp)
         polygon_exp = np.array([A_exp, B_exp, C_exp, D_exp], np.int32)
         #print("im here")
         #breakpoint()
-        #rr, cc = draw.polygon(polygon_exp[:, 1], polygon_exp[:, 0], tresh_matrix.shape)
-        #tresh_matrix[rr, cc] = 1
+        #rr, cc = draw.polygon(polygon_exp[:, 1], polygon_exp[:, 0], thresh_mask.shape)
+        #thresh_mask[rr, cc] = 1
 
 
-        cv2.fillConvexPoly(tresh_matrix, polygon_exp, (255, 255, 255))
+        cv2.fillConvexPoly(thresh_mask, polygon_exp, 1)
     #print("okok")
     return shapes
-
 # Function to draw a triangle on the matrix
-def draw_triangle(matrix, mask, shapes, top_left, size, angle=0, tresh_matrix = None, threshold=300, placed=None):
+def draw_triangle(matrix, mask, shapes, top_left, size, angle=0, thresh_mask = None, threshold=300, placed=None):
 
     x, y = top_left
     if size % 2 != 0:
@@ -199,9 +261,9 @@ def draw_triangle(matrix, mask, shapes, top_left, size, angle=0, tresh_matrix = 
     # if y-height < threshold or x+size > matrix.shape[0]-threshold or x < threshold:
         # matrix += 1000
 
-    A = (x, y)
-    B = (x + size, y)
-    C = (int(x + size / 2), y + height)
+    A = (x + size / 2, y + height) # bottom left
+    B = (x, y) # top vertex
+    C = (x + size, y) # bottom right
     #print('#')
     #print(A, B, C)
 
@@ -227,34 +289,33 @@ def draw_triangle(matrix, mask, shapes, top_left, size, angle=0, tresh_matrix = 
 
     shapes.append(["triangle", polygon, angle, center])
 
-    if tresh_matrix is not None:
+    if thresh_mask is not None:
 
         A_exp, B_exp, C_exp = expand_geometric_figure([A_rot, B_rot, C_rot], 1000)
         #print(A_exp, B_exp, C_exp)
 
         # test if any of the expanded points is too close to the border
-        if any(x < threshold or x > tresh_matrix.shape[0] - threshold or y < threshold or y > tresh_matrix.shape[1] - threshold for x, y in [A_exp, B_exp, C_exp]):
-            tresh_matrix += 1000
+        if any(x < threshold or x > thresh_mask.shape[0] - threshold or y < threshold or y > thresh_mask.shape[1] - threshold for x, y in [A_exp, B_exp, C_exp]):
+            thresh_mask += 1000
 
         polygon = np.array([A_exp, B_exp, C_exp], np.int32)
-        #cv2.line(tresh_matrix, A_exp, B_exp, color=(255, 255, 255), thickness=10)
-        #cv2.line(tresh_matrix, A_exp, C_exp, color=(255, 255, 255), thickness=10)
-        #cv2.line(tresh_matrix, B_exp, C_exp, color=(255, 255, 255), thickness=10)
-        cv2.fillConvexPoly(tresh_matrix, polygon, (255, 255, 255))
+        cv2.fillConvexPoly(thresh_mask, polygon, 1)
 
     return shapes
 
 # Function to draw a pentagon on the matrix
 
-def draw_pentagon(matrix, mask, shapes, center, size, angle=0, tresh_matrix=None, threshold=300, placed=None):
+def draw_pentagon(matrix, mask, shapes, center, size, angle=0, thresh_mask=None, threshold=300, placed=None):
     x_center, y_center = center
-    default_pos_angle = -90 + 180  # Align pentagon to the x-axis
+    default_pos_angle = 90  # Align pentagon to the x-axis
+
+    radius = size / (2 * np.sin(np.pi / 5))
 
     vertices = []
     for i in range(5):
         theta = np.radians(default_pos_angle + i * 360 / 5)  # Distribute vertices evenly around the center
-        x = int(x_center + (size*0.75) * np.cos(theta))
-        y = int(y_center + (size*0.75) * np.sin(theta))
+        x = int(x_center + radius * np.cos(theta))
+        y = int(y_center + radius * np.sin(theta))
         vertices.append((x, y))
 
     #print(vertices)
@@ -269,7 +330,6 @@ def draw_pentagon(matrix, mask, shapes, center, size, angle=0, tresh_matrix=None
     #print(vertices_rotated)
 
 
-
     for i in range(5):
         x1, y1 = vertices_rotated[i]
         x2, y2 = vertices_rotated[(i + 1) % 5]
@@ -281,7 +341,7 @@ def draw_pentagon(matrix, mask, shapes, center, size, angle=0, tresh_matrix=None
     cv2.fillConvexPoly(mask, polygon, 1)
     shapes.append(["pentagon", polygon, angle, center])
 
-    if tresh_matrix is not None:
+    if thresh_mask is not None:
 
         vertices_exp = expand_geometric_figure(vertices_rotated, 1000)
         #print(vertices_exp)
@@ -289,14 +349,14 @@ def draw_pentagon(matrix, mask, shapes, center, size, angle=0, tresh_matrix=None
         #for i in range(5):
             #x1, y1 = vertices_exp[i]
             #x2, y2 = vertices_exp[(i + 1) % 5]
-            #cv2.line(tresh_matrix, (x1, y1), (x2, y2), color=(255, 255, 255), thickness=10)
+            #cv2.line(thresh_mask, (x1, y1), (x2, y2), color=(255, 255, 255), thickness=10)
 
-        if any(x < threshold or x > tresh_matrix.shape[0] - threshold or y < threshold or y > tresh_matrix.shape[1] -
+        if any(x < threshold or x > thresh_mask.shape[0] - threshold or y < threshold or y > thresh_mask.shape[1] -
                threshold for x, y in vertices_exp):
-            tresh_matrix += 1000
+            thresh_mask += 1000
 
         polygon = np.array(vertices_exp, np.int32)
-        cv2.fillConvexPoly(tresh_matrix, polygon, (255, 255, 255))
+        cv2.fillConvexPoly(thresh_mask, polygon, 1)
 
     return shapes
 
@@ -311,12 +371,12 @@ def can_place(matrix, mask, shape_fn, top_left, size,angle):
         return False
     # temp_matrix = np.copy(matrix)
     temp_matrix = np.zeros(matrix.shape)
-    temp_tresh_matrix = np.zeros(matrix.shape)
     temp_mask = np.zeros(mask.shape)
+    temp_thresh_mask = np.zeros(mask.shape)
     temp_shapes = []
-    temp_shapes = shape_fn(temp_matrix, temp_mask, temp_shapes, top_left, size, angle, temp_tresh_matrix)
+    temp_shapes = shape_fn(temp_matrix, temp_mask, temp_shapes, top_left, size, angle, temp_thresh_mask)
     # breakpoint()
-    return np.max(temp_tresh_matrix + matrix) <= 255
+    return np.max(temp_thresh_mask + mask) <= 1
     # return np.sum(temp_matrix) == np.sum(matrix) + size * size
 
 
@@ -330,10 +390,11 @@ def place_shape(matrix, mask, shapes, shape_fn, size,angle=0):
         y = random.randint(0, matrix.shape[1]-1)
         if can_place(matrix, mask, shape_fn, (x, y), size, angle):
             placed = True
+            # angle = 0
             shapes = shape_fn(matrix, mask, shapes,(x, y), size,angle, placed=placed)
             # print the shape characteristics
             print(f'Placed {shapes[-1][0]} at {shapes[-1][3]} with a rotation of {angle}° and a size of {size}.')
-            plt.imshow(matrix)
+            plt.imshow(matrix,origin='lower')
             plt.show()
     return shapes
 
@@ -346,10 +407,10 @@ def generate_matrix():
 
     shapes = []
 
-    num_shapes = random.randint(5, 9)
+    num_shapes = random.randint(5, 8)
     #num_shapes = 1
     print(num_shapes)
-    shape_functions = [draw_square, draw_triangle, draw_pentagon]
+    shape_functions = [draw_square, draw_rectangle, draw_triangle, draw_pentagon]
     shape_sizes = [random.randint(800, 1200) for _ in range(num_shapes)]  # Example size range
     #breakpoint()
     for size in shape_sizes:
